579b3f509c499e1b0c5c1dc3eb66a5882216b080
Evan Older <evanmolder@gmail.com>
2023-06-01 18:49:23 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,3 +1,4 @@
+import geni.portal as portal
 import geni.rspec.pg as rspec
 
 # Create a Request object to start building the RSpec.
@@ -9,4 +10,4 @@ node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD"
 node.routable_control_ip = "true"
 
 # Print the RSpec to the enclosing page.
-portal.context.printRequestRSpec()import geni.portal as portal
+portal.context.printRequestRSpec()
