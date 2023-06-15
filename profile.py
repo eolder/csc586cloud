a68072fdb87aaa69ff7f4041d5ca17817094292c
Evan Older <evanmolder@gmail.com>
2023-06-15 18:42:24 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -15,7 +15,7 @@ for i in range(2):
     node = request.XenVM("host")
     
   node.routable_control_ip = "true"
-  node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD
+  node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD"
   iface = node.addInterface("if" + str(i))
   iface.component_id = "eth1"
   iface.addAddress(rspec.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0")
