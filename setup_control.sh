9b2df899a6cc637f5b7aef75e7df00cfbe96bcdb
Evan Older <evanmolder@gmail.com>
2023-06-15 19:35:57 -0400

    Create setup_control.sh

---
+++ setup_control.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+runuser -u eo925456 -- ssh-keygen -q -t rsa -f /users/eo925456/.ssh/id_rsa -N ''
+runuser -u eo925456 -- cat /users/eo925456/ssh/id_rsa.pub >> /users/eo925456/.ssh/authorized_keys
