4fc419393d37d1587dced4043bc23ac0d0e7e0c8
Evan Older <evanmolder@gmail.com>
2023-06-15 19:28:22 -0400

    Update setup_control.sh

--- setup_control.sh
+++ setup_control.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-su eo925456
-cd ~
-ssh-keygen -q -t rsa -f .ssh/id_rsa -N ''
-cat .ssh/id_rsa.pub >> .ssh/authorized_keys
+runuser -u eo925456 ssh-keygen -q -t rsa -f /users/eo925456/.ssh/id_rsa -N ''
+runuser -u eo925456 cat .ssh/id_rsa.pub >> /users/eo925456/.ssh/authorized_keys
